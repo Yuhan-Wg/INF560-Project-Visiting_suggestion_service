976874f1fe6f4780f0d500d3d971eda65e4de518
yuhanwg <yuhanwg1996@gmail.com>
2019-04-08 19:30:35 -0700

    Update matrix input in cnn

--- src/model/produce_matrix.py
+++ src/model/produce_matrix.py
@@ -5,7 +5,7 @@ from copy import deepcopy
 # This script contains function tranforming dataset presenting in blocks to matrix
 # to features and labels for CNN training.
 
-def matrixTrans(df, level, between=1):
+def matrixTrans(df, level, between=7):
     """
     Useful columns:
     Level	latBlock	lngBlock	count
@@ -14,24 +14,36 @@ def matrixTrans(df, level, between=1):
     tf = deepcopy(df.loc[df['Level'] == level])
     latMax = tf.latBlock.max()+1
     lngMax = tf.lngBlock.max()+1
-    tf["order"] = tf.month * 10**6+ tf.day *10**4 + tf.hour*10**2+tf.minute
+    tf["order"] = tf.month * 10**2+ tf.day
     orders = sorted(tf["order"].unique())
-    features = np.zeros((len(orders),lngMax,latMax))
-    labels = np.zeros((len(orders),lngMax,latMax))
-    index = 0
-    for i in range(len(orders)-between):
-        prev = tf.loc[tf.order==orders[i]]
-        next = tf.loc[tf.order==orders[i+between]]
-        if prev.day.max() != next.day.max():
-            continue
-        # If in the same day
-        for _, row in prev.iterrows():
-            features[index, row['lngBlock'],row['latBlock']] = row['count']
-        for _,row in next.iterrows():
-            labels[index, row['lngBlock'],row['latBlock']] = row['count']
-        index += 1
+
+    def generator():
+        for hour in tf.hour.unique():
+            for minute in tf.minute.unique():
+                yield hour,minute
+
+    g = generator()
+    num = 0
+    for h,m in g:
+        num+= len(orders)-between
+    features = np.zeros((num,lngMax,latMax,between))
+    labels = np.zeros((num,lngMax,latMax))
+
+    g = generator()
+    for hour,minute in g:
+        temp = tf.loc[(tf.hour==hour)&(tf.minute==minute)]
+        for i in range(between, len(orders)):
+            index = 0
+            next = temp.loc[tf.order==orders[i]]
+            for _,row in next.iterrows():
+                labels[index, row['lngBlock'],row['latBlock']] = row['count']
+            for b in range(between):
+                prev = temp.loc[tf.order==orders[i-1-b]]
+                for _, row in prev.iterrows():
+                    features[index, row['lngBlock'],row['latBlock'],b] = row['count']
+            index += 1
 
     del tf["order"]
-    features = features[:index,:,:]
+    features = features[:index,:,:,:]
     labels = labels[:index,:,:]
     return features, labels
